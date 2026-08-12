fe44c6b5ae899a5bb5db55204973c7be2cd7104e
Hilmi Arizal <hilmiarizal@gmail.com>
2026-08-12 10:28:57 +0700

    update-limit-vercel

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ connectDB().then(async (isConnected) => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Ensure MongoDB Atlas Connection on Serverless Executions
 app.use(async (req, res, next) => {
