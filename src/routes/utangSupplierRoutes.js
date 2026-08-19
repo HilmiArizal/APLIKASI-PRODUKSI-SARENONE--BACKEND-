67aa0471764c337cf94528ce0f05d0db895fd2f7
Hilmi Arizal <hilmiarizal@gmail.com>
2026-08-19 18:54:18 +0700

    fix: Register POST /clear-all and DELETE /clear-all routes at top of router

--- src/routes/utangSupplierRoutes.js
+++ src/routes/utangSupplierRoutes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/utangSupplierController');
 
+// Dedicated Clear Routes (Must be BEFORE :id)
+router.delete('/clear/all', controller.clearAll);
+router.delete('/clear-all', controller.clearAll);
+router.post('/clear-all', controller.clearAll);
+
 router.get('/', controller.getAll);
 router.post('/', controller.create);
 router.post('/:id/pay', controller.pay);
 router.post('/:id/receive', controller.receive);
-router.delete('/clear/all', controller.clearAll);
-router.delete('/all', controller.clearAll);
 router.delete('/:id', controller.remove);
 
 module.exports = router;
